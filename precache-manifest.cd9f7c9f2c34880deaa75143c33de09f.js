bd1294915819a3f5320129922c0dcbaaba5f19eb
alebelcor <856838+alebelcor@users.noreply.github.com>
2019-05-19 15:16:24 -0700

    Updates

---
+++ precache-manifest.cd9f7c9f2c34880deaa75143c33de09f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "140d55aca7c0751087c1",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.a5f725ed.js"
+  },
+  {
+    "revision": "a4829d6f4f86e1aaebe4",
+    "url": "/react-tic-tac-toe/static/js/main.dffc31d7.chunk.js"
+  },
+  {
+    "revision": "8b99aff82b1f3fded115",
+    "url": "/react-tic-tac-toe/static/js/2.3267a43e.chunk.js"
+  },
+  {
+    "revision": "a4829d6f4f86e1aaebe4",
+    "url": "/react-tic-tac-toe/static/css/main.2e198359.chunk.css"
+  },
+  {
+    "revision": "9be10d422bba8f9e0e73a7ff032bde3c",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
